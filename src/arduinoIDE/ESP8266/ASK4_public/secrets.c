44b8159313b0d6a8c8d73c1bdaf3d535a3cdeaad
jakura4life <aliff2.azizulazily@live.uwe.ac.uk>
2025-10-30 21:04:53 +0000

    Initial commit: Hydroponic monitoring system

---
+++ src/arduinoIDE/ESP8266/ASK4_public/secrets.c
@@ -0,0 +1,5 @@
+#include "secrets.h"
+
+const char* WIFI_SSID = "ASK4 Wireless";
+const char* FIREBASE_URL = "https://console.firebase.google.com/project/hydroponic-7fd3b/database/hydroponic-7fd3b-default-rtdb/data/~2F";
+const char* FIREBASE_KEY = "cFYcvsVCAIKJayoOu7PZe1MOykhNqEqG21fMs4aY";
---
+++ src/arduinoIDE/ESP8266/ASK4_public/secrets.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+extern const char* WIFI_SSID;
+extern const char* FIREBASE_URL;
+extern const char* FIREBASE_KEY;
+
+#endif
